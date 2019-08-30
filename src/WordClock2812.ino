af2300e462ffaedc98bfee634bfabf0c735299fc
David Weese <dave.weese@gmail.com>
2019-08-30 14:13:01 +0200

    add more sensors

--- src/WordClock2812.ino
+++ src/WordClock2812.ino
@@ -366,14 +366,128 @@ void setup() {
     //The values for the gain and integration times are written transfered to the sensor through the function config_TSL2591
     //This function powers the device ON, then configures the sensor and finally powers the device OFF again 
        
-    tsl2591.config_TSL2591();  
+    tsl2591.config_TSL2591();
+    
+    
+
+    //*********************************************************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //Now choose on which mode your device will run
+    //On doubt, just leave on normal mode, that's the default value
+    
+    //0b00:     In sleep mode no measurements are performed, but power consumption is at a minimum
+    //0b01:     In forced mode a single measured is performed and the device returns automatically to sleep mode
+    //0b11:     In normal mode the sensor measures continually (default value)
+    
+    bme280.parameter.sensorMode = 0b11;                   //Choose sensor mode
+    
+    
+    
+    //*********************** TSL2591 *************************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //Great! Now set up the internal IIR Filter
+    //The IIR (Infinite Impulse Response) filter suppresses high frequency fluctuations
+    //In short, a high factor value means less noise, but measurements are also less responsive
+    //You can play with these values and check the results!
+    //In doubt just leave on default
+    
+    //0b000:      factor 0 (filter off)
+    //0b001:      factor 2
+    //0b010:      factor 4
+    //0b011:      factor 8
+    //0b100:      factor 16 (default value)
+    
+    bme280.parameter.IIRfilter = 0b100;                    //Setup for IIR Filter
+    
+    
+    
+    //************************** BME280 ***********************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //Next you'll define the oversampling factor for the humidity measurements
+    //Again, higher values mean less noise, but slower responses
+    //If you don't want to measure humidity, set the oversampling to zero
+    
+    //0b000:      factor 0 (Disable humidity measurement)
+    //0b001:      factor 1
+    //0b010:      factor 2
+    //0b011:      factor 4
+    //0b100:      factor 8
+    //0b101:      factor 16 (default value)
+    
+    bme280.parameter.humidOversampling = 0b101;            //Setup Humidity Oversampling
+    
+    
+    
+    //************************** BME280 ***********************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //Now define the oversampling factor for the temperature measurements
+    //You know now, higher values lead to less noise but slower measurements
+    
+    //0b000:      factor 0 (Disable temperature measurement)
+    //0b001:      factor 1
+    //0b010:      factor 2
+    //0b011:      factor 4
+    //0b100:      factor 8
+    //0b101:      factor 16 (default value)
+    
+    bme280.parameter.tempOversampling = 0b101;             //Setup Temperature Ovesampling
+    
+    
+    
+    //************************** BME280 ***********************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //Finally, define the oversampling factor for the pressure measurements
+    //For altitude measurements a higher factor provides more stable values
+    //On doubt, just leave it on default
+    
+    //0b000:      factor 0 (Disable pressure measurement)
+    //0b001:      factor 1
+    //0b010:      factor 2
+    //0b011:      factor 4
+    //0b100:      factor 8
+    //0b101:      factor 16 (default value)
+    
+    bme280.parameter.pressOversampling = 0b101;            //Setup Pressure Oversampling 
+    
+    
+    
+    //************************** BME280 ***********************************
+    //*************ADVANCED SETUP - SAFE TO IGNORE!************************
+    
+    //For precise altitude measurements please put in the current pressure corrected for the sea level
+    //On doubt, just leave the standard pressure as default (1013.25 hPa)
+    
+    bme280.parameter.pressureSeaLevel = 1013.25;           //default value of 1013.25 hPa
+    
+    //Now write here the current average temperature outside (yes, the outside temperature!)
+    //You can either use the value in Celsius or in Fahrenheit, but only one of them (comment out the other value)
+    //In order to calculate the altitude, this temperature is converted by the library into Kelvin
+    //For slightly less precise altitude measurements, just leave the standard temperature as default (15°C)
+    //Remember, leave one of the values here commented, and change the other one!
+    //If both values are left commented, the default temperature of 15°C will be used
+    //But if both values are left uncommented, then the value in Celsius will be used    
+    
+    bme280.parameter.tempOutsideCelsius = 20;              //default value of 20°C
+    //bme280.parameter.tempOutsideFahrenheit = 59;           //default value of 59°F
+
+
 
     //*********************************************************************
     //*************ADVANCED SETUP IS OVER - LET'S CHECK THE CHIP ID!*******
     
+    if (bme280.init_BME280() != 0x60) {
+        debug("Ops! BME280 could not be found!");
+    } else {
+        debug("BME280 detected!");
+    }
+    
     if (tsl2591.init_TSL2591() != 0x50) {
         debug("Ops! TSL2591 could not be found!");
-        //while(1);
     } else {
         debug("TSL2591 detected!");
     }
